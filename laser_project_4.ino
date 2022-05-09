b503d857f3a81fdf32131e38b1e84992cd491612
MrEndermen <peewee.gogo@gmail.com>
2022-05-09 14:26:06 -0700

    Update laser_project_4.ino

--- laser_project_4.ino
+++ laser_project_4.ino
@@ -36,7 +36,8 @@ void loop() {
   {
     digitalWrite(LED, HIGH); // if the console to read below 400 turn the LED on
     delay(1000); // wait 1 seconds to wait for beam to establish
-// There is an issue in this area. The console stops pushing data when it drops below 400. I need it to continue or the alarm goes off.
+// There is an issue in this area. The console stops pushing data when it drops below 400. 
+// I need it to continue or the alarm goes off because no new numbers have shown up in the console that it's reading from. it just stopped and the previous readout is still the current readout. 
     if (light < 400) // if the console does not say above 400 turn on the alarm.
     {
       alarm(); // sound the alarm
